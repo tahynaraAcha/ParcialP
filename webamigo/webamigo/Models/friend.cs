1be17f7c6f9623ca92fec31ca3b0387bad8b195d
tahynaritafay@gmail.com <tahynaritafay@gmail.com>
2019-09-27 20:30:11 -0400

    DataContext

---
+++ webamigo/webamigo/Models/friend.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace webamigo.Models
+{
+    public enum TypeList
+    {
+        Amalia,
+        Amanda,
+        Angel,
+        Andres,
+        Angelina
+
+    }
+    public class friend
+    {
+        [Required]
+        public string Name { get; set; }
+        [Key]
+        public TypeList List { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Birthday { get; set; }
+
+
+
+
+    }
+}
